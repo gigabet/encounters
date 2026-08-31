62a9081d771087e1704be1e1a7cc80d808f3a220
gigabet <47139205+gigabet@users.noreply.github.com>
2026-08-31 15:19:38 +0300

    incom

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
-import type { NextConfig } from "next";
+import createMDX from '@next/mdx'
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   reactCompiler: true,
-};
+}
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
